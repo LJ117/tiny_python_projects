97ea64c67416223140a64cb2414cff7ad71ca579
seven <lj7seven@foxmail.com>
2023-08-11 23:35:50 +0800

    feat: finished ch18

--- 18_gematria/gematria.py
+++ 18_gematria/gematria.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
-Author : Me <me@foo.com>
-Date   : today
+Author : seven <seven@localhost>
+Date   : 2023-08-11
 Purpose: Gematria
 """
 
 import argparse
 import os
+import re
+from functools import reduce
 
 
 # --------------------------------------------------
@@ -17,9 +19,9 @@ def get_args():
         description="Gematria", formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    parser.add_argument("text", metavar="text", help="Inout text of file")
-
+    parser.add_argument("text", metavar="text", help="Input text of file")
     args = parser.parse_args()
+
     if os.path.isfile(args.text):
         args.text = open(args.text).read().rstrip()
 
@@ -27,11 +29,38 @@ def get_args():
 
 
 # --------------------------------------------------
+def word2num(word):
+    vals = map(ord, re.sub("[^a-zA-Z0-9]", "", word))
+
+    word_sum = sum(vals)
+
+    return str(word_sum)
+
+
+# --------------------------------------------------
+def test_word2num():
+    """
+    Test word2num
+    """
+    assert word2num("a") == "97"
+    assert word2num("abc") == "294"
+    assert word2num("ab'c") == "294"
+    assert word2num("4a-b'c,") == "346"
+
+
+# --------------------------------------------------
 def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    print(args.text)
+    # for line in str(args.text).splitlines():
+    #     print(" ".join(map(word2num, line.split())))
+
+    for line in str(args.text).splitlines():
+        new_line = list()
+        for word in line.split():
+            new_line.append(word2num(word))
+        print(" ".join(new_line))
 
 
 # --------------------------------------------------
